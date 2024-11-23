8d777d6e8cc7fcaeb9f6e6295399bf6693869b26
war2v <war2v@mtmail.mtsu.edu>
2024-11-23 17:38:11 -0600

    Added reponsive Nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Nav from './components/Nav'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Nav></Nav>
+    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/SignIn" element={<SignIn/>}/>
+      <Route path="/SignUp" element={<SignUp/>}/>
+      <Route path="/Profile" element={<Profile/>}/>
+      <Route path="/About" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Nav from './Nav'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-slate-800 shadow-md text-white p-2">
+        <Nav />
+    </header>
+  )
+}
